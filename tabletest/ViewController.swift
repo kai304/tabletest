10ed39dcfb3d448dfa5c11d737a4cedc844c7054
kato kaiu <kaiu.kato39@gmail.com>
2020-02-23 10:36:25 +0900

    Initial Commit

---
+++ tabletest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabletest
+//
+//  Created by 加藤海宇 on 2020/02/23.
+//  Copyright © 2020 加藤海宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
